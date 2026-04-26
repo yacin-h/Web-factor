14dd120c56814899a5c1dda36e233ca82d0bb73f
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-04-26 09:10:36 +0330

    fix: remove unused google fonts

--- app/root.tsx
+++ app/root.tsx
@@ -16,19 +16,7 @@ import { Toaster } from "@/components/ui/sonner";
 import type { Route } from "./+types/root";
 import { TooltipProvider } from "./components/ui/tooltip";
 
-export const links: Route.LinksFunction = () => [
-    { rel: "preconnect", href: "https://fonts.googleapis.com" },
 
-    {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossOrigin: "anonymous",
-    },
-    {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-    },
-];
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
