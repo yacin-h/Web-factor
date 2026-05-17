f12cb625eb9dff430fa68db11128561226682b8b
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-05-17 10:41:10 +0330

    ♻️refactor(new invoice) :extract select invoice status into reusable component

---
+++ app/features/invoices/components/invoiceForm/invoiceStatusSelect.tsx
@@ -0,0 +1,42 @@
+import { Controller, useFormContext } from "react-hook-form";
+
+import { Label } from "@/features/shared/components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/features/shared/components/ui/select";
+
+import type { InvoiceFormType } from "../../schema/invoice.schema";
+const {
+    control,
+    formState: { errors },
+} = useFormContext<InvoiceFormType>();
+export default function InvoiceStatusSelect() {
+    return (
+        <div className="space-y-3 ">
+            <Label htmlFor="status">وضعیت پرداخت</Label>
+            <Controller
+                control={control}
+                name="status"
+                render={({ field }) => (
+                    <Select value={field.value} onValueChange={field.onChange}>
+                        <SelectTrigger className=" text-right px-3 py-2 border rounded-md">
+                            <SelectValue placeholder="وضعیت پرداخت" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="pending">در انتظار</SelectItem>
+                            <SelectItem value="paid">پرداخت شده</SelectItem>
+                            <SelectItem value="cancelled">لغو شده</SelectItem>
+                        </SelectContent>
+                    </Select>
+                )}
+            />
+            {errors.status && (
+                <span className="text-red-500">{errors.status.message}</span>
+            )}
+        </div>
+    );
+}
