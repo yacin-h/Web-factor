1c211d7479d112b819aad5d3f8a8ab948cd5f3f8
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-06-13 09:58:47 +0330

    fix(footer) : fix wrong social link

--- app/features/home/components/footer.tsx
+++ app/features/home/components/footer.tsx
@@ -67,7 +67,7 @@ export function Footer() {
                             <li ><a href="https://t.me/webfactor_ir" target="_blank">کانال تلگرام</a></li>
                         </ul>
                         <ul className="flex gap-1.5 items-center space-y-2 text-sm">
-                            <li ><a href="https://t.me/webfactor_ir" target="_blank">کانال بله</a></li>
+                            <li ><a href="https://ble.ir/webfactor" target="_blank">کانال بله</a></li>
                         </ul>
                         </div>
                         
